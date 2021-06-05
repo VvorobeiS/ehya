b70611791f21fe0446469e22ba76de31225ae339
VvorobeiS <mr.vorob@bk.ru>
2021-06-06 00:38:28 +0600

    create: section books & fixed all

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,6 +42,59 @@ const categoriesSlider = new Swiper('.categories-slider__container', {
   navigation: {
     nextEl: '.categories-slider__btn--next',
     prevEl: '.categories-slider__btn--prev',
+    disabledClass: 'categories-slider__btn--disabled'
+  },
+
+  // Управление клавиатурой
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  // Автопрокрутка
+  // autoplay: {
+  //   delay: 5000,
+  // Отключить после ручного переключения
+  //   disableOnInteraction: false,
+  // },
+});
+
+const booksSlider = new Swiper('.books-slider__container', {
+  // Optional parameters
+  loop: false,
+  slidesPerView: 1,
+  speed: 700,
+
+  // Настройки адаптивной версии
+  breakpoints: {
+    // если браузер >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // если браузер >= 320px
+    577: {
+      slidesPerView: 2,
+    },
+    // если браузер >= 768px
+    769: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    // если браузер >= 1200px
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 28
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.books-slider__btn--next',
+    prevEl: '.books-slider__btn--prev',
+    disabledClass: 'books-slider__btn--disabled',
   },
 
   // Управление клавиатурой
