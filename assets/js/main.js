e5909468ab03053fcdab534f0c46c9a6dddc64ef
VvorobeiS <mr.vorob@bk.ru>
2021-06-05 18:15:20 +0600

    create: section categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,55 @@ $(document).ready(function () {
     $('.nav').toggleClass('nav--visible');
   });
 
+const categoriesSlider = new Swiper('.categories-slider__container', {
+  // Optional parameters
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 28,
+  speed: 700,
+
+  // Настройки адаптивной версии
+  breakpoints: {
+    // если браузер >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      slidesPerColumn: 2,
+      slidesPerColumnFill: 'row',
+    },
+    // если браузер >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    // если браузер >= 1200px
+    1200: {
+      slidesPerView: 4,
+      slidesPerColumn: 1,
+      slidesPerColumnFill: 'column',
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.categories-slider__btn--next',
+    prevEl: '.categories-slider__btn--prev',
+  },
+
+  // Управление клавиатурой
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  // Автопрокрутка
+  // autoplay: {
+  //   delay: 5000,
+  // Отключить после ручного переключения
+  //   disableOnInteraction: false,
+  // },
+});
 })
